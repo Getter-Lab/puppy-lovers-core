78bf8143d6d3786cbcc331ec282a579c38693829
Verissimo Santos <jose.sacramento@grupoboticario.com.br>
2023-11-20 14:56:26 -0300

    chore: config application input validator

--- src/example/dto/create-example.dto.ts
+++ src/example/dto/create-example.dto.ts
@@ -1,1 +1,7 @@
-export class CreateExampleDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateExampleDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly teste: string;
+}
