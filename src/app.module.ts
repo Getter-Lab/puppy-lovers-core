148becae35b493f0a0a5000d9d1ecbaa8693f869
Verissimo Santos <jose.sacramento@grupoboticario.com.br>
2023-11-20 15:49:51 -0300

    chore: config application environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ExampleModule } from './example/example.module';
 
+const ENVIRONMENT = process.env.NODE_ENV || 'development';
+
 @Module({
-  imports: [ExampleModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${ENVIRONMENT}`
+    }),
+    ExampleModule
+  ],
   controllers: [],
   providers: [],
 })
